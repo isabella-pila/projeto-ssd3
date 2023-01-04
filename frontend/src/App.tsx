fe9652312b3f2a8cbb7d0b740953f3ad79c9ac4c
isabella pila <isabellapilasilva@gmail.com>
2023-01-04 20:05:55 -0300

    components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
-
-
+import DataTable from "components/DataTable";
+import Footer from "components/footer";
+import NavBar from "components/NavBar";
 
 function App() {
   return (
-    <div>
+    <>
+      <NavBar />
+      <div className="container" >
 
-      <h1 className="text-primary">ola mundo</h1>
-    </div>
+        <h1 className="text-primary">ola mundo</h1>
+<DataTable/>
+      </div>
+      <Footer/>
+
+      </>
   );
 }
 
---
+++ frontend/src/components/footer/index.tsx
@@ -0,0 +1,18 @@
+
+const Footer = () => {
+    return (
+      
+        <footer className="footer mt-auto py-3 bg-dark">
+        <div className="container">
+            <p className="text-light">App desenvolvido por <a href="https://github.com/isabella-pila" target="_blank" rel="noreferrer">Isabella Pila Silva</a></p>
+            <p className="text-light"><small><strong>dog's & cat's</strong><br />
+                contato: <a href="https://instagram.com/everton_p_s" target="_blank" rel="noreferrer">@isabella</a></small></p>
+        </div>
+    </footer>
+);
+
+}
+
+
+
+export default Footer;
