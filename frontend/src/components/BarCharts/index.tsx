48e2803c781b8dba8c2ca8df129245215927f4ee
isabella pila <isabellapilasilva@gmail.com>
2023-01-05 11:35:51 -0300

    barCharts

--- frontend/src/components/BarCharts/index.tsx
+++ frontend/src/components/BarCharts/index.tsx
@@ -1,4 +1,5 @@
-/*import axios from 'axios';
+/*
+import axios from 'axios';
 import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSuccess } from 'types/sale';
